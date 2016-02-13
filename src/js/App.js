5fb1094fca953735ffdee32a9456a3df1dda4b00
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-13 15:16:47 -0800

    mergy

--- src/js/App.js
+++ src/js/App.js
@@ -2,23 +2,43 @@
 
 // React.render(<Parent />, document.getElementById('app'));
 
-      var UserGist = React.createClass({
+var BearApp = React.createClass({
       getInitialState: function() {
         return {
-          username: '',
-          lastGistUrl: ''
+          bears: []
         };
       },
 
       componentDidMount: function() {
         this.serverRequest = $.get(this.props.source, function (result) {
-          var lastGist = result[0];
+          console.log(result[0]);
+          var bears = result;
+          console.log(bears);
           this.setState({
-            username: lastGist.owner.login,
-            lastGistUrl: lastGist.html_url
+            bears: bears
           });
         }.bind(this));
       },
+      
+      // handleCommentSubmit: function(comment) {
+      //   $.ajax({
+      //     url: this.props.source,
+      //     dataType: 'json',
+      //     type: 'POST',
+      //     data: comment,
+      //     success: function(data) {
+      //       this.setState({data: data});
+      //     }.bind(this),
+      //     error: function(xhr, status, err) {
+      //       console.error(this.props.url, status, err.toString());
+      //     }.bind(this)
+      //   });
+      // },
+
+      //  componentDidMount: function() {
+      //     this.loadCommentsFromServer();
+      //     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+      //   },
 
       componentWillUnmount: function() {
         this.serverRequest.abort();
@@ -26,15 +46,26 @@
 
       render: function() {
         return (
-          <div>
-            {this.state.username}s last gist is
-            <a href={this.state.lastGistUrl}>here</a>.
-          </div>
-        );
-      }
-    });
 
-    React.render(
-      <UserGist source="https://api.github.com/users/octocat/gists" />,
-      document.getElementById('app')
-    );
+          <div>
+            {
+              this.state.bears.map(function(bear) {
+                return <section key={bear._id}> <p> name: {bear.name}</p>  <p>fishPreference: {bear.fishPreference} </p> </section>
+              })
+            }
+          </div>
+          
+          // <div className="commentBox">
+          //   <h1>Comments</h1>
+          //   <CommentList data={this.state.data} />
+          //   <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+          // </div>
+
+          );
+        }
+      });
+
+      ReactDOM.render(
+        <BearApp source="http://localhost:3000/api/bears" />,
+        document.getElementById('app')
+      );
